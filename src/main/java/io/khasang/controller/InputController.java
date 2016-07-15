e89dc416a1a20ec9172e34928e8884186510bbc8
browseup <an@browseup.ru>
2016-07-15 15:52:29 +0300

    Добавел пакет view и controller с пустыми классами

---
+++ src/main/java/io/khasang/controller/InputController.java
@@ -0,0 +1,7 @@
+package io.khasang.controller;
+
+/**
+ * Created by alexey_n on 15.07.2016.
+ */
+public class InputController {
+}
---
+++ src/main/java/io/khasang/view/ConsoleView.java
@@ -0,0 +1,7 @@
+package io.khasang.view;
+
+/**
+ * Created by alexey_n on 15.07.2016.
+ */
+public class ConsoleView {
+}
